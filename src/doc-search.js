4eeeaedb920bc6d07834a7b5054123eca7221433
Hailey Gaylor <haileygaylor@gmail.com>
2019-09-13 11:53:59 -0700

    working on returning values with API key

--- src/doc-search.js
+++ src/doc-search.js
@@ -1,10 +1,10 @@
 // Back-end
 
-export class GiphyGenerator {
-  searchGifs(keyword) {
+export class DocSearch {
+  searchparameters(state,symptoms) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=WA&query=headache&ski=0&limit=10&user_key=${process.env.API_KEY}`; // watch for back ticks.
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${state}&query=${symptoms}&ski=0&limit=10&user_key=${process.env.API_KEY}`; //  back ticks.
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -17,21 +17,21 @@ export class GiphyGenerator {
     });
   }
 }
-
-export class Dictionary{
-  getDefinition(word) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${process.env.OTHER_API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
-}
+//
+// export class Dictionary{
+//   getDefinition(word) {
+//     return new Promise(function(resolve, reject) {
+//       let request = new XMLHttpRequest();
+//       const url = `https://dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${process.env.OTHER_API_KEY}`;
+//       request.onload = function() {
+//         if (this.status === 200) {
+//           resolve(request.response);
+//         } else {
+//           reject(Error(request.statusText));
+//         }
+//       }
+//       request.open("GET", url, true);
+//       request.send();
+//     });
+//   }
+// }
--- src/main.js
+++ src/main.js
@@ -2,61 +2,77 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Dictionary } from './doc-search';
-import { GiphyGenerator } from './doc-search';
+import { DocSearch } from './doc-search';
 
 $(document).ready(function() {
   $('#submit').click(function() {
-    let userWord = $("#searchWord").val();
-    let defWords = [];
+    const symptoms = $("#userInput").val();
+    const state = $("#userState").val();
+      $("#UserState").val("");
+      $("#userSymptoms").val("");
 
 
-    let defSearch = new Dictionary();
-    let giphyGenerator = new GiphyGenerator(); // create instance of WeatherService class. Change these values to custom your page.
-    let promise = defSearch.getDefinition(userWord);  // call the instance method and pass in user input.
+        let results = new DocSearch();
+        let promise = results.searchparameters(state,symptoms);  // call the instance method and pass in user input
+        console.log(promise);
 
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      console.log(body);
+ promise.then(function(response) {
+   const body = JSON.parse(response);
+   $('.doctor').text(`The humidity in ${state} is ${body.main.humidity}%`);
+   $('.location').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
 
-      //defWords = body[0].meta.syns[0];
-      for(let i=0; i<body.length; i++) {
-        for(let j=0; j<body[i].meta.syns.length; j++) {
-          for(let k=0; k<body[i].meta.syns[j].length; k++) {
-            defWords.push(body[i].meta.syns[j][k]);
-          }
-        }
-      }
-      console.log(defWords);
-
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
-
-    promise.then(function() {
-      $('.giphy').html("");
-      for(let i=0; i<defWords.length; i++) {
-
-        let promise2 = giphyGenerator.searchGifs(defWords[i]);
-
-        promise2.then(function(response) {
-          const body = JSON.parse(response);
-          //target the img value and display with a img src to display
-
-          $('.giphy').append(`<img src=${body.data[0].images.original.url}>`);
-
-
-        }, function(error) {
-          $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-        });
-      }
-
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
-
-
-
-  });
+ }, function(error) {
+   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+ });
+});
 
 });
+
+
+
+  //   let defSearch = new Dictionary();
+  //   let giphyGenerator = new GiphyGenerator(); // create instance of WeatherService class. Change these values to custom your page.
+  //   let promise = defSearch.getDefinition(userWord);  // call the instance method and pass in user input.
+  //
+  //   promise.then(function(response) {
+  //     const body = JSON.parse(response);
+  //     console.log(body);
+  //
+  //     //defWords = body[0].meta.syns[0];
+  //     for(let i=0; i<body.length; i++) {
+  //       for(let j=0; j<body[i].meta.syns.length; j++) {
+  //         for(let k=0; k<body[i].meta.syns[j].length; k++) {
+  //           defWords.push(body[i].meta.syns[j][k]);
+  //         }
+  //       }
+  //     }
+  //     console.log(defWords);
+  //
+  //   }, function(error) {
+  //     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+  //   });
+  //
+  //   promise.then(function() {
+  //     $('.giphy').html("");
+  //     for(let i=0; i<defWords.length; i++) {
+  //
+  //       let promise2 = giphyGenerator.searchGifs(defWords[i]);
+  //
+  //       promise2.then(function(response) {
+  //         const body = JSON.parse(response);
+  //         //target the img value and display with a img src to display
+  //
+  //         $('.giphy').append(`<img src=${body.data[0].images.original.url}>`);
+  //
+  //
+  //       }, function(error) {
+  //         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+  //       });
+  //     }
+  //
+  //   }, function(error) {
+  //     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+
+  //
+  //
+  //
