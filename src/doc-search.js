cf999dad33b9375ac7292defc79dc7085464c8ae
Hailey Gaylor <haileygaylor@gmail.com>
2019-09-25 21:37:23 -0700

    attempt to display info to user.

--- src/doc-search.js
+++ src/doc-search.js
@@ -1,10 +1,9 @@
 // Back-end
-
 export class DocSearch {
   searchparameters(state,symptoms) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${state}&query${symptoms}&ski=0&limit=10&user_key=${process.env.API_KEY}`; //  back ticks.
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${state}&query=${symptoms}&ski=0&limit=10&user_key=004a80f5404d2972245862bea267d63a`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -17,21 +16,4 @@ export class DocSearch {
     });
   }
 }
-//
-// export class Dictionary{
-//   getDefinition(word) {
-//     return new Promise(function(resolve, reject) {
-//       let request = new XMLHttpRequest();
-//       const url = `https://dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${process.env.OTHER_API_KEY}`;
-//       request.onload = function() {
-//         if (this.status === 200) {
-//           resolve(request.response);
-//         } else {
-//           reject(Error(request.statusText));
-//         }
-//       }
-//       request.open("GET", url, true);
-//       request.send();
-//     });
-//   }
-// }
+//API Key not working in .env =(
--- src/main.js
+++ src/main.js
@@ -17,79 +17,22 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      for(let i=0; i < 10; i++) {
-        providerInfo[i] = body.data[i].practices;
+      console.log(body);
+      for(let i=0; i < 5; i++) {
+        providerInfo = body.data[].profile.forEach(function(doctor)
+          console.log(providerInfo);
+          //gather provider info based on users search selection
+        //display each provider info to the user
 
-        $("#location").append(`<div id=q${i} class="question col-md-4">
-         <h4 class="category">${body.results[i].category}</h4>
-         <h3 class="points">$${scoring[scoring.indexOf(body.results[i].difficulty)+1]}</h3>
-       </div><br>`);
-      }
-      console.log(providerInfo);
-      // console.log(body.data[0].practices[0].visit_address);
-// body.data[].practices[].accepts_new_patients
-// body.data[].practices[].visit_address
-// body.data[].practices[].phones
-// body.data[].practices[].name
-// body.data[].practices[].office_hours
-
-       $('.doctor').html( `Displaying a list of providers in ${state} ,  ${body.data[0,1].profile}`);
+          $('.doctor').html( `Displaying a list of providers in ${state},${body.data[].profile}`);
+          $('.providerLocation').text("")
 
 
-
-
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+            (function(error) {
+              $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+            })
+          })
+        }
+      })
+    })
   });
-
-});
-
-
-
-//   let defSearch = new Dictionary();
-//   let giphyGenerator = new GiphyGenerator(); // create instance of WeatherService class. Change these values to custom your page.
-//   let promise = defSearch.getDefinition(userWord);  // call the instance method and pass in user input.
-//
-//   promise.then(function(response) {
-//     const body = JSON.parse(response);
-//     console.log(body);
-//
-//     //defWords = body[0].meta.syns[0];
-//     for(let i=0; i<body.length; i++) {
-//       for(let j=0; j<body[i].meta.syns.length; j++) {
-//         for(let k=0; k<body[i].meta.syns[j].length; k++) {
-//           defWords.push(body[i].meta.syns[j][k]);
-//         }
-//       }
-//     }
-//     console.log(defWords);
-//
-//   }, function(error) {
-//     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-//   });
-//
-//   promise.then(function() {
-//     $('.giphy').html("");
-//     for(let i=0; i<defWords.length; i++) {
-//
-//       let promise2 = giphyGenerator.searchGifs(defWords[i]);
-//
-//       promise2.then(function(response) {
-//         const body = JSON.parse(response);
-//         //target the img value and display with a img src to display
-//
-//         $('.giphy').append(`<img src=${body.data[0].images.original.url}>`);
-//
-//
-//       }, function(error) {
-//         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-//       });
-//     }
-//
-//   }, function(error) {
-//     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-
-//
-//
-//
