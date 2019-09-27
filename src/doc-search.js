3a5de1b238777c569b3cf5ecbe878701419018da
Hailey Gaylor <haileygaylor@gmail.com>
2019-09-26 23:15:31 -0700

    trying to put the doctor info and display it as a list to the user

--- src/doc-search.js
+++ src/doc-search.js
@@ -3,7 +3,7 @@ export class DocSearch {
   searchparameters(state,symptoms) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${state}&query=${symptoms}&ski=0&limit=10&user_key=004a80f5404d2972245862bea267d63a`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${state}&query=${symptoms}&ski=0&limit=10&user_key=${process.env.API_KEY}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -16,4 +16,4 @@ export class DocSearch {
     });
   }
 }
-//API Key not working in .env =(
+//API Key not working in .env =( 004a80f5404d2972245862bea267d63a
--- src/main.js
+++ src/main.js
@@ -8,31 +8,51 @@ $(document).ready(function() {
   $('#submit').click(function() {
     const symptoms = $("#userSymptoms").val();
     const state = $("#userState").val();
-    let providerInfo=[];
 
 
-    let results = new DocSearch();
-    let promise = results.searchparameters(state,symptoms);  // call the instance method and pass in user input
 
+    let docSearch = new DocSearch();
+    let promise = docSearch.searchparameters(state,symptoms);  // call the instance method and pass in user input
 
     promise.then(function(response) {
+      $('#searchResults').show();
+      $('#searchResults .message').html('Displaying a list of providers in ' + state + ' specializing with ' + symptoms + ' .')
       const body = JSON.parse(response);
-      console.log(body);
-      for(let i=0; i < 5; i++) {
-        providerInfo = body.data[].profile.forEach(function(doctor)
-          console.log(providerInfo);
-          //gather provider info based on users search selection
-        //display each provider info to the user
 
-          $('.doctor').html( `Displaying a list of providers in ${state},${body.data[].profile}`);
-          $('.providerLocation').text("")
+      // Iterate over each profile.
+      for(let counter = 0; counter < 5; counter++) {
+        var rowOutput = '';
 
+        if (body.data[counter] != undefined) {
+          var doctor = body.data[counter];
 
-            (function(error) {
-              $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-            })
-          })
+          // Get doctor full name name
+          var name = doctor.profile.first_name + ' ' + doctor.profile.last_name;
+          rowOutput = '<li>' + name;
+
+          // Generate list of locations
+          if (Array.isArray(doctor.practices)) {
+            console.log('Execution!');
+            // rowOutput += ''
+            rowOutput = rowOutput.concat('<ul><li>Locations</li></ul>');
+            // console.log('Start loop');
+            // doctor.practices.forEach(function(location) {
+            //   console.log('In loop');
+            //   // console.log(location)
+            //   rowOutput += '<li>Location Test</li>';
+            // });
+            // console.log('End loop');
+
+          }
+          rowOutput += '</li>';
+
+          $('#searchResults ul').append(rowOutput);
         }
-      })
-    })
+      }
+    });
   });
+});
+
+// (function(error) {
+//   $('.showErrors').text('There was an error processing your request: ${error.message}');
+// });
